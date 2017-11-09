6d149eb16083939599932cf07a6a8894902f4427
Carlo Clementucci <cclementucci@gmail.com>
2017-11-09 12:25:47 -0500

    ready_timeout fix without breaking previous work

--- lib/chef/provisioning/vsphere_driver/driver.rb
+++ lib/chef/provisioning/vsphere_driver/driver.rb
@@ -889,9 +889,8 @@ module ChefProvisioningVsphere
         else
           ## Check if true available
           vm_ip = bootstrap_options[:customization_spec][:ipsettings][:ip] unless vm_helper.ip?
-          ready_timeout = bootstrap_options[:ready_timeout] || 90
           nb_attempts = 0
-          until @vm_helper.open_port?(vm_ip, @vm_helper.port, 1) || nb_attempts > ready_timeout
+          until @vm_helper.open_port?(vm_ip, @vm_helper.port, 1) || (nb_attempts > (bootstrap_options[:ready_timeout] || 90))
             print '.'
             nb_attempts += 1
           end
